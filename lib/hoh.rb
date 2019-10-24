9bcb78c6127016c61569f8eeb4b9355e6f5c3a9f
burbage stacey <rstacey06@gmail.com>
2019-10-24 17:05:37 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -60,8 +60,7 @@ def naming_system
   # Remember:
 
  
-  #  Genus
-  #  Species
+  #
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
 
